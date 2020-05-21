2369672cc7fe87350dd748da5017816d85fb94be
Prabesh Pokharel <praveas@gmail.com>
2020-05-21 12:14:07 +1000

    Updates

---
+++ precache-manifest.bccd41f9641abcad788d6b1c880be875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2792cecacb19e8911814c5ce5b0e0e6d",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "49c7789bcd27f4008832",
+    "url": "/react-deploy/static/css/main.91d9c748.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c7789bcd27f4008832",
+    "url": "/react-deploy/static/js/main.e0d5b221.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "c59eb6d624fac728609d355091753c64",
+    "url": "/react-deploy/static/media/P1012271.c59eb6d6.JPG"
+  }
+]);
